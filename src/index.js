e6b7dcbe7df265e47bc241a7f15816c4b0efce68
Olivier Lafleur <olafleur@godaddy.com>
2022-04-26 11:11:42 -0400

    Add sidebar (video 2 - 4:32)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert( 'yo' );
+const { registerPlugin } = wp.plugins;
+const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
+
+registerPlugin( 'prefix-gutenberg-sidebar', {
+    icon: 'smiley',
+    render: () => {
+        return (
+            <>
+                <PluginSidebar
+                    name="prefix-gutenberg-sidebar"
+                    title= { 'A title' } >
+                        Sidebar Content.
+                </PluginSidebar>
+            </>
+        )
+    }
+} );
