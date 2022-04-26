afc15373443a4eab01b3e39d6a402286b830a526
Olivier Lafleur <olafleur@godaddy.com>
2022-04-26 11:05:07 -0400

    Initial commit

---
+++ reproduction-giorgi.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: Reproduction Giorgi
+ * Description : A fun plugin to reproduce a bug
+ * Version: 1.0.0
+ */
+
+if( !defined( 'ABSPATH' ) ) exit();
+
+// Enqueue
+function enqueue_assets() {
+    wp_enqueue_script(
+        'gutenberg-sidebar',
+        plugins_url( 'build/index.js', __FILE__ ),
+        [ 'wp-plugins', 'wp-edit-post', 'wp-i18n', 'wp-element', 'wp-components', 'wp-data']
+    );
+}
+
+add_action( 'enqueue_block_editor_assets', 'enqueue_assets' );
